13d71311d995d097cf340b2359a20d94eb69a80f
Vicente Salazar <v.salazar11@ufromail.cl>
2025-11-11 01:06:11 -0300

    aa

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask import request
+from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/hello")
+@app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/hello")
+def hello():
+    name = request.args.get("name", "Flask")
+    return f"Hello, {escape(name)}!"
